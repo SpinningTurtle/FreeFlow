43d6ae1086c8f5d9524c8954ed1253bf68e45baa
Dan Saglam <danielyigitsaglam@hotmail.com>
2025-06-22 15:13:53 +0100

    Removed sepia

--- my-nuxt-app/pages/index.vue
+++ my-nuxt-app/pages/index.vue
@@ -9,8 +9,8 @@
     <h1>Color mode: {{ $colorMode.value }}</h1>
     <select v-model="$colorMode.preference">
       <option value="system">System</option>
-      <option value="dark">Dark</option>
       <option value="light">Light</option>
+      <option value="dark">Dark</option>
     </select>
   </div>
 </template>
