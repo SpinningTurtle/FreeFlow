5c6f45d7fd92af480ceffc2175cfcda587a9aa76
Dan Saglam <danielyigitsaglam@hotmail.com>
2025-06-22 14:48:15 +0100

    Routed app.vue to index.vue, removed old test ts files

---
+++ my-nuxt-app/pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+        <h1>Welcome to My Nuxt App</h1>
+        <p>This is the home page of my Nuxt application.</p>
+        <NuxtLink to="/about">Go to About Page</NuxtLink>
+    </div>
+</template>
