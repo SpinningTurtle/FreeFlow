1c4469466a69305c4fb00678cab44715f802acf0
Dan Saglam <danielyigitsaglam@hotmail.com>
2025-06-22 15:05:39 +0100

    Added dark mode to index

--- my-nuxt-app/pages/index.vue
+++ my-nuxt-app/pages/index.vue
@@ -4,4 +4,35 @@
         <p>This is the home page of my Nuxt application.</p>
         <NuxtLink to="/about">Go to About Page</NuxtLink>
     </div>
+
+    <div>
+    <h1>Color mode: {{ $colorMode.value }}</h1>
+    <select v-model="$colorMode.preference">
+      <option value="system">System</option>
+      <option value="light">Light</option>
+      <option value="dark">Dark</option>
+      <option value="sepia">Sepia</option>
+    </select>
+  </div>
 </template>
+
+<script setup>
+const colorMode = useColorMode()
+
+console.log(colorMode.preference)
+</script>
+
+<style>
+body {
+  background-color: #fff;
+  color: rgba(0,0,0,0.8);
+}
+.dark-mode body {
+  background-color: #091a28;
+  color: #ebf4f1;
+}
+.sepia-mode body {
+  background-color: #f1e7d0;
+  color: #433422;
+}
+</style>
